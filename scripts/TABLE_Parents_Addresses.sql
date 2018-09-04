48b54f38107c3041ca661566cc6e2405722a66fc
HugoRoca <hugo.rock20@hotmail.com>
2018-09-04 18:15:45 -0500

    TABLE_Student_Addresses.sql

--- scripts/TABLE_Parents_Addresses.sql
+++ scripts/TABLE_Parents_Addresses.sql
@@ -3,5 +3,5 @@ parent_id int foreign key references Parents(parent_id),
 address_id int foreign key references Addresses(address_id),
 date_address_from date,
 date_address_to date
-primary key(parent_id, address_id, date_address_from)
+primary key(date_address_from, parent_id, address_id)
 )
---
+++ scripts/TABLE_Student_Addresses.sql
@@ -0,0 +1,8 @@
+create table Student_Addresses(
+student_id int foreign key references Students(student_id),
+address_id int foreign key references Addresses(address_id),
+date_from date,
+date_to date,
+address_details text
+primary key(date_from, student_id, address_id)
+)
