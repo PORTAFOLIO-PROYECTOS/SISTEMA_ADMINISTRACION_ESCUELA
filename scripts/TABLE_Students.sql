39b544b8bdd51b280567de1852f9763c5bfb31d3
HugoRoca <hugo.rock20@hotmail.com>
2018-09-04 18:09:22 -0500

    TABLE_Students.sql

---
+++ scripts/TABLE_Students.sql
@@ -0,0 +1,10 @@
+create table Students(
+student_id int,
+gender varchar(20),
+first_name varchar(100),
+middle_name varchar(100),
+last_name varchar(100),
+date_of_birth date,
+other_student_details text
+primary key(student_id)
+)
