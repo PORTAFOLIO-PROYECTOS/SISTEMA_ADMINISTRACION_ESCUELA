aad723c2733e93457ad0deb61eccc65cfc3e65cb
HugoRoca <hugo.rock20@hotmail.com>
2018-09-04 17:38:28 -0500

    TABLE_Teachers.sql

---
+++ scripts/TABLE_Teachers.sql
@@ -0,0 +1,10 @@
+create table Teachers(
+teacher_id int,
+school_id int foreign key references Schools(school_id),
+gender varchar(20),
+firts_name varchar(100),
+middle_name varchar(100),
+last_name varchar(100),
+other_teacher_details text
+primary key (teacher_id)
+)
