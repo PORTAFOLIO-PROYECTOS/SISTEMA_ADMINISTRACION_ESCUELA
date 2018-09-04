f73ea32b8d4664acc8df68a9e001f625ab1dc6ed
HugoRoca <hugo.rock20@hotmail.com>
2018-09-04 17:51:15 -0500

    TABLE_Parents.sql

---
+++ scripts/TABLE_Parents.sql
@@ -0,0 +1,9 @@
+create table Parents(
+parent_id int, 
+gender varchar(20),
+first_name varchar(100),
+middle_name varchar(100),
+last_name varchar(100),
+other_parent_details text
+primary key (parent_id)
+)
--- scripts/TABLE_Parents_Addresses.sql
+++ scripts/TABLE_Parents_Addresses.sql
@@ -1,5 +1,5 @@
 create table Parents_Addresses(
-parent_id int,
+parent_id int foreign key references Parents(parent_id),
 address_id int foreign key references Addresses(address_id),
 date_address_from date,
 date_address_to date
