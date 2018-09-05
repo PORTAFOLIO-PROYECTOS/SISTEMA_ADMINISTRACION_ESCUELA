7ffdf7ced8ba0094c83c75401c320cec4d9a3a83
HugoRoca <hugo.rock20@hotmail.com>
2018-09-05 10:23:07 -0500

    text -> varchar(max)

--- scripts/TABLE_Teachers.sql
+++ scripts/TABLE_Teachers.sql
@@ -5,6 +5,6 @@ gender varchar(20),
 firts_name varchar(100),
 middle_name varchar(100),
 last_name varchar(100),
-other_teacher_details text
+other_teacher_details varchar(max)
 primary key (teacher_id)
 )
