26bf256c9077c0451cde27c03ab2198e1922b88d
Hugo <hugo.rock20@hotmail.com>
2018-09-16 23:02:48 -0500

    add entitt/Student_Class.cs

---
+++ EscuelaOnline/EscuelaOnline.Entity/Student_Class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscuelaOnline.Entity
+{
+    public class Student_Class
+    {
+        public int student_id { get; set; }
+        public int class_id { get; set; }
+        public DateTime date_from { get; set; }
+        public DateTime date_to { get; set; }
+    }
+}
