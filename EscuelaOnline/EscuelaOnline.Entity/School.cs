aade0e7102343ae6198875319ac90753a477d940
Hugo <hugo.rock20@hotmail.com>
2018-09-16 22:48:01 -0500

    add Entity/School.cs

---
+++ EscuelaOnline/EscuelaOnline.Entity/School.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscuelaOnline.Entity
+{
+    public class School
+    {
+        public int school_id { get; set; }
+        public int addres_id { get; set; }
+        public string school_name { get; set; }
+        public string school_principal { get; set; }
+        public string other_school_details { get; set; }
+    }
+}
