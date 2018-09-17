0c05e269b8e9c5538cb8d5843b20df5230536e82
Hugo <hugo.rock20@hotmail.com>
2018-09-16 22:59:08 -0500

    add entity/Subject.cs

---
+++ EscuelaOnline/EscuelaOnline.Entity/Subject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscuelaOnline.Entity
+{
+    public class Subject
+    {
+        public int subject_id { get; set; }
+        public string subject_name{ get; set; }
+    }
+}
