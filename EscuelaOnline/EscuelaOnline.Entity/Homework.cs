06baf8c8751d67275182193d8960c9244df850a1
Hugo <hugo.rock20@hotmail.com>
2018-09-19 23:45:22 -0500

    add Entity/Homework.cs

---
+++ EscuelaOnline/EscuelaOnline.Entity/Homework.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscuelaOnline.Entity
+{
+    public class Homework
+    {
+        public int homework_id { get; set; }
+        public int student_id { get; set; }
+        public DateTime date_created { get; set; }
+        public string homework_content { get; set; }
+        public string grade { get; set; }
+        public string other_homework_details { get; set; }
+    }
+}
