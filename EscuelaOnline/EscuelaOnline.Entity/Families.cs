a7fe7e0f8757908b6687e61743afef6ef90992e3
Hugo <hugo.rock20@hotmail.com>
2018-09-19 23:39:13 -0500

    add entity/Families.cs

---
+++ EscuelaOnline/EscuelaOnline.Entity/Families.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscuelaOnline.Entity
+{
+    public class Families
+    {
+        public int family_id { get; set; }
+        public int head_of_family_parent { get; set; }
+        public string family_name { get; set; }
+    }
+}
