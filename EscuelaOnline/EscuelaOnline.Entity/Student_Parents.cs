940c81891371870f1137fef3525d2c73f93ea619
Hugo <hugo.rock20@hotmail.com>
2018-09-19 23:43:34 -0500

    add entity/Student_Parents.cs

---
+++ EscuelaOnline/EscuelaOnline.Entity/Student_Parents.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscuelaOnline.Entity
+{
+    public class Student_Parents
+    {
+        public int student_id { get; set; }
+        public int parent_id { get; set; }
+    }
+}
