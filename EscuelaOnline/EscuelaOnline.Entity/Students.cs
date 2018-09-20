0a3fd2eb68ff1f9a431fe73920f47c80b866a0fb
Hugo <hugo.rock20@hotmail.com>
2018-09-19 23:47:38 -0500

    add entity/Students.cs

---
+++ EscuelaOnline/EscuelaOnline.Entity/Students.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscuelaOnline.Entity
+{
+    public class Students
+    {
+        public int student_id { get; set; }
+        public string gender { get; set; }
+        public string first_name { get; set; }
+        public string middle_name { get; set; }
+        public string last_name { get; set; }
+        public DateTime date_of_birth { get; set; }
+        public string other_student_details { get; set; }
+    }
+}
